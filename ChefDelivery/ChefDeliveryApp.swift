c8a18c232e5c4927af6473e9978cdde1943fbe99
Wagner Duarte <46401074+WagnerofDuarte@users.noreply.github.com>
2023-07-19 18:15:55 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Wagner Duarte on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
